c625270d4fe8e82f71a4ae6b569b502752f771e5
Phil Hawksworth <phil@netlify.com>
2022-05-26 18:59:25 +0100

    Reset for intro demo

--- src/site/_includes/js/fetch-joke.js
+++ src/site/_includes/js/fetch-joke.js
@@ -2,5 +2,7 @@ fetch('.netlify/functions/joke')
   .then(response => response.json())
   .then(json => {
     const element = document.querySelector('#joke');
-    element.innerHTML = json.msg;
+    if (element) {
+      element.innerHTML = json.msg;
+    }
   });
